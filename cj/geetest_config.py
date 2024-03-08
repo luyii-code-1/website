344d3a0c28e06d01e1b2469ceb0c27cfd6129dbe
luyii-code-1 <112700386+luyii-code-1@users.noreply.github.com>
2024-03-08 19:17:12 +0800

    del

--- cj/geetest_config.py
+++
@@ -1,7 +0,0 @@
-GEETEST_ID = "797b0fe80a4fbb48bb7103a41442a54d"
-GEETEST_KEY = "cc7ac7d4f4a13ddd6fd6fb1f1dfbecdd"
-REDIS_HOST = "127.0.0.1"
-REDIS_PORT = "6379"
-CYCLE_TIME = 10
-GEETEST_BYPASS_STATUS_KEY = "gt_server_bypass_status"
-BYPASS_URL = "http://bypass.geetest.com/v1/bypass_status.php"
